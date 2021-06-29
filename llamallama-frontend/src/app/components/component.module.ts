7fe457ec1aaf6dbc8e85aeee8a099fa6717ded62
Raul Montenegro <draul99@gmail.com>
2021-06-29 16:24:16 -0500

    Update component.module.ts

--- llamallama-frontend/src/app/components/component.module.ts
+++ llamallama-frontend/src/app/components/component.module.ts
@@ -4,24 +4,47 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductosComponent } from './productos/productos.component';
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import { CrudProductosComponent } from './crud-productos/crud-productos.component';
 import { BusquedaComponent } from './busqueda/busqueda.component';
+import { AddComponent } from './dialogs/add/add.component';
+import { DeleteComponent } from './dialogs/delete/delete.component';
+import { EditComponent } from './dialogs/edit/edit.component';
+import { AddtiendaproductoComponent } from './dialogstiendaproducto/addtiendaproducto/addtiendaproducto.component';
+import { EdittiendaproductoComponent } from './dialogstiendaproducto/edittiendaproducto/edittiendaproducto.component';
+import { DeletetiendaproductoComponent } from './dialogstiendaproducto/deletetiendaproducto/deletetiendaproducto.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
     BusquedaComponent
+    AddComponent,
+    DeleteComponent,
+    EditComponent,
+    AddtiendaproductoComponent,
+    EdittiendaproductoComponent,
+    DeletetiendaproductoComponent,
 
   ],
   imports:[
     CommonModule,
     FormsModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   exports: [
     LoginComponent,
